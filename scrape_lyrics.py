50e2aeb97f2af71b63402c5e16220f8184a8aae1
Dhruv Joshi <dhruv.joshi.1989@gmail.com>
2016-11-10 06:51:59 +0000

    removed img tag, now will parse as list of strings

--- scrape_lyrics.py
+++ scrape_lyrics.py
@@ -10,12 +10,16 @@ song_name = 'remember'
 uri = 'http://www.songlyrics.com/' + artist_hyphenated + '/' + song_name + '-lyrics/'
 response = requests.get(uri)
 
-print response.status_code
-
 if response.status_code == 200:
 	x  = response.text
 	y = BeautifulSoup(x, "html.parser")
 	z = y.findAll("p", { "id" : "songLyricsDiv" })
 	# re-parse as html
 	soup = BeautifulSoup(str(z[0]), 'html.parser')
-	print soup.prettify()
+	# remove the annoying ad <img> tags
+	soup.img.extract()
+	# will attempt to remove <br> tags
+	print soup
+	# print soup.prettify()
+else:
+	print "Could not find song"
