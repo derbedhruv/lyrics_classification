ca5beb435100d4627c4295512cc3004492a41071
Dhruv Joshi <dhruv.joshi.1989@gmail.com>
2016-11-11 06:15:45 +0000

    can get pages properly by scanning the html

--- scrape_lyrics.py
+++ scrape_lyrics.py
@@ -4,24 +4,44 @@ import requests
 from bs4 import BeautifulSoup
 
 # de-capitalize names of artist and songs, replace spaces with hyphens
-artist_hyphenated = 'aerosmith' 
-song_name = 'remember'
+def get_song_lyrics(artist, song):
+	artist_hyphenated = 'aerosmith' 
+	song_name = 'remember'
 
-uri = 'http://www.songlyrics.com/' + artist_hyphenated + '/' + song_name + '-lyrics/'
+	uri = 'http://www.songlyrics.com/' + artist_hyphenated + '/' + song_name + '-lyrics/'
+	response = requests.get(uri)
+
+	if response.status_code == 200:
+		x  = response.text
+		y = BeautifulSoup(x, "html.parser")
+		z = y.findAll("p", { "id" : "songLyricsDiv" })
+		# re-parse as html
+		soup = BeautifulSoup(str(z[0]), 'html.parser')
+		# remove the annoying ad <img> tags
+		img = soup.img.extract()
+		# convert to text and split at newline
+		sentence_case_lyrics = soup.get_text().split('\n')
+		for a in sentence_case_lyrics[6:]:
+			print a
+		# print soup.prettify()
+	else:
+		print "Could not find song"
+
+# scrape the whole website slowly
+uri = 'http://www.songlyrics.com/a/'
 response = requests.get(uri)
+x  = response.text
+y = BeautifulSoup(x, "html.parser")
+z = y.findAll("li", { "class" : "li_pagination" })
+# find length of z to know the breadth
+zz = str(z[1]).split('\n')
+# Now we can parse this.. need to go from  zz[1] to zz[n-2]
+soup = BeautifulSoup(str(z[1]), 'html.parser')
 
-if response.status_code == 200:
-	x  = response.text
-	y = BeautifulSoup(x, "html.parser")
-	z = y.findAll("p", { "id" : "songLyricsDiv" })
-	# re-parse as html
-	soup = BeautifulSoup(str(z[0]), 'html.parser')
-	# remove the annoying ad <img> tags
-	img = soup.img.extract()
-	# convert to text and split at newline
-	sentence_case_lyrics = soup.get_text().split('\n')
-	for a in sentence_case_lyrics[6:]:
-		print a
-	# print soup.prettify()
-else:
-	print "Could not find song"
+
+for p in y.findAll("li", { "class" : "li_pagination" })[1:-1]:
+  current_link = str(p.find('a'))
+  curr = BeautifulSoup(current_link, 'html.parser')
+  link = curr.find('a')
+  if not link == None:
+    print link['href']
