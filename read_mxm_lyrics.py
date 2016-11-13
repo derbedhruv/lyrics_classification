083e108406a7a922ec288606bbd4d48fb2c09778
Dhruv Joshi <derbedhruv@Dhruvs-MacBook-Pro.local>
2016-11-13 15:24:33 -0800

    modularized code for reading in text, now will run NLTK to train models

--- read_mxm_lyrics.py
+++ read_mxm_lyrics.py
@@ -18,6 +18,62 @@ def predict_genre(weights, x):
 	_, i = max((dotProduct(weight, x), i) for i, weight in enumerate(weights))
 	return genre_labels[i]
 
+def read_genres(filename):
+	"""
+	@param filename: The location of the file containing genre information. Should have each line of the form:
+	TRAAAAK128F9318786	Rock
+	TRAAAFD128F92F423A      Punk    Rock
+	Where the first is track_id and the second is the Genre
+	Reads the file containing genre clasifications, returns a dict containing track_id <--> genre mappings
+	One track_id can be mapped to multiple genres, these are entered as a list
+	**NOTE: The header and endline is hardcoded
+	"""
+	print "reading genre classifications ...",
+	# read in all genre classifications into a dict 
+	# 15 genres are 'Reggae', 'Latin', 'RnB', 'Jazz', 'Metal', 'Pop', 'Punk', 'Country', 'New Age', 'Rap', 'Rock', 'World', 'Blues', 'Electronic', 'Folk'
+	genre = {}
+	f = open(filename, 'r')
+	for _ in range(7,280838):	# skip header of 7 lines, go till end of 280838 lines
+	  data = f.readline().strip()
+	  data = data.split('\t')
+	  genre[data[0]] = tuple(x for x in data[1:])		# put the data into the genre dict
+	f.close()
+	print "done!"
+	return genre
+
+def extract_data(words, start, end, f, song_list):
+	"""
+	Returns song data as given in the file format - track_id, 
+	"""
+	for i in range(start, end):
+		d = defaultdict(int)
+		ligne = f.readline()
+		ligne = ligne.split(',')
+		track_id = ligne[0]
+		mxm_id = ligne[1]
+		for j in range(2,len(ligne)):
+		  # put rest of the line into the defaultdict
+		  wordclass = ligne[j]
+		  w_no, w_count = map(int, wordclass.split(':'))
+		  d[words[w_no-1]] = w_count	# word index starts from 1!
+		# append the (track_id, mxm_id, defaultdict) to the list of songs
+		song_list.append((track_id, mxm_id, d))
+
+def prepare_set(genre, songs):
+	"""
+	@param genre: A dict of genres, as returned by read_genres()
+	@param songs: A list of songs, as returned by extract_data()
+	Combines the genres and songs into a form which is parse-able by NLTK, which is a list of tuples (song_dict, genre)
+	"""
+	tset = []
+	for song in songs:
+		try:
+			for g in genre[song[0]]:
+				tset.append((song[2], g))	# take the first ([0]'th) genre of the genres that the current song identifies with
+		except KeyError:
+			continue
+	return tset
+
 def read_data_BoW():
 	"""
 	Read in files from the dataset and put them into a (training_set, testing_set) tuple
@@ -28,7 +84,7 @@ def read_data_BoW():
 	f_train = open('mxm_dataset_train.txt', 'r')
 
 	for _ in range(17):
-	  # vaska first 17 header lines
+	  # ignore first 17 header lines
 	  f_train.readline()
 	words = f_train.readline()
 	words = words.split(',')
@@ -36,77 +92,30 @@ def read_data_BoW():
 	# read songs into sparse vector
 	print "reading songs for training...",
 	training_songs = []	# list of songs, each represented by a defaultdict
-	for i in range(18, 210537):
-	  d = defaultdict(int)
-	  ligne = f_train.readline()
-	  ligne = ligne.split(',')
-	  track_id = ligne[0]
-	  mxm_id = ligne[1]
-	  for j in range(2,len(ligne)):
-		  # put rest of the line into the defaultdict
-		  wordclass = ligne[j]
-		  w_no, w_count = map(int, wordclass.split(':'))
-		  d[words[w_no-1]] = w_count	# word index starts from 1!
-	  # append the (track_id, mxm_id, defaultdict) to the list of songs
-	  training_songs.append((track_id, mxm_id, d))
+	extract_data(words, 18, 210537, f_train, training_songs)
 	f_train.close()
 	print "done!",
-	print "reading genre classifications ...",
-	# read in all genre classifications into a dict 
-	# 15 genres are 'Reggae', 'Latin', 'RnB', 'Jazz', 'Metal', 'Pop', 'Punk', 'Country', 'New Age', 'Rap', 'Rock', 'World', 'Blues', 'Electronic', 'Folk'
-	genre = {}
-	f = open('msd_tagtraum_cd2.cls', 'r')
-	for _ in range(7,280838):	# skip header of 7 lines, go till end of 280838 lines
-	  data = f.readline().strip()
-	  data = data.split('\t')
-	  genre[data[0]] = tuple(x for x in data[1:])		# put the data into the genre dict
-	f.close()
-	print "done!",
 
 	# Now combine both into a training set
-	training_set = []
-	for song in training_songs:
-		try:
-			for g in genre[song[0]]:
-				training_set.append((song[2], g))	# take the first ([0]'th) genre of the genres that the current song identifies with
-		except KeyError:
-			continue
+	training_set = prepare_set(genre, training_songs)
 
-	print "reading songs for training...",
+	print "reading songs for testing...",
 	f_test = open('mxm_dataset_test.txt', 'r')
-
-	for _ in range(17):
-	  # vaska first 18 header lines
+	for _ in range(18):
+	  # ignore first 18 header lines
 	  f_test.readline()
-	words = f_test.readline()
-	words = words.split(',')
 
 	testing_songs = []	# list of songs, each represented by a defaultdict
-	for i in range(18, 27161):
-	  d = defaultdict(int)
-	  ligne = f_test.readline()
-	  ligne = ligne.split(',')
-	  track_id = ligne[0]
-	  mxm_id = ligne[1]
-	  for j in range(2,len(ligne)):
-		  # put rest of the line into the defaultdict
-		  wordclass = ligne[j]
-		  w_no, w_count = map(int, wordclass.split(':'))
-		  d[words[w_no-1]] = w_count	# word index starts from 1!
-	  # append the (track_id, mxm_id, defaultdict) to the list of songs
-	  testing_songs.append((track_id, mxm_id, d))
-	f_test.close()
-	print "done!"
+	extract_data(words, 18, 27161, f_test, testing_songs)
+	f_train.close()
+
 	# Now combine both into a training set
-	testing_set = []
-	for song in testing_songs:
-		try:
-			for g in genre[song[0]]:
-				testing_set.append((song[2], g))	# take the first ([0]'th) genre of the genres that the current song identifies with
-		except KeyError:
-			continue
+	testing_set = prepare_set(genre, testing_songs)
+
+	# return both train and test sets as tuple
 	return (training_set, testing_set)
 
 ''' ------------------------------------------------------------------------------------------------ '''
 
 # Now will use nltk to train a model
+genre = read_genres('msd_tagtraum_cd2.cls')
