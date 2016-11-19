d6fd6c169a995cd2fb1d2a96357867ac1fca8065
Dhruv Joshi <derbedhruv@Dhruvs-MacBook-Pro.local>
2016-11-18 19:23:29 -0800

    added stochastic grad descent into util

--- util.py
+++ util.py
@@ -38,3 +38,73 @@ def ngram(song, n = 2):
 	"""
 	ngrams = nltk.ngrams(song.split(), n)
 	return ngrams
+
+# 5. The old stochastic gradient descent for all-vs-one classification
+def stochastic_grad_descent(training_set, genres, numIters=10, eta=0.01):
+	'''
+	@param training_set: 
+	@param genres:
+	@param numIters: number of iterations to run, 10 is usually more than enough
+	@param eta: step size for stochastic gradient descent, can play around to find optimal
+	Given training_set, which is a list of (track_id, mxm_id, vector) tuples. 
+	The 'vector' is a sparse vector containing number of times a word occurs
+	in a song's lyrics.
+	This function will return the weight vector (sparse
+	feature vector) learned using stochastic gradient descent.
+	'''
+	weights = [{} for _ in range(len(genre_labels))]
+	D = len(training_songs)
+	random.seed(88)
+	def loss(xx, yy, weights):
+		# the hinge loss in 0-1 prediction of xx as yy
+		out = 0
+		for i, weight in enumerate(weights):
+		     # return 1 if it is the genre corresponding to this weight, else return -1
+		     if yy[0] == genre_labels[i]:
+		             y = 1
+		     else:
+		             y = -1
+		     # find hinge loss for each genre vector
+		     out += max(0, 1 - y*dotProduct(xx, weight))
+		return out
+         		
+	def increment_weight(xx, yy, weights):
+		# use the increment() function to make things convenient
+		for i, weight in enumerate(weights):
+			# return 1 if it is the genre corresponding to this weight, else return -1
+			if yy[0] == genre_labels[i]:
+				y = 1
+			else:
+				y = -1
+			if y*dotProduct(weight, xx) < 1:
+				increment(weight, eta*y, xx)
+	for i in range(numIters):
+		# calculate loss function with current vector 'weights'
+		lossFunc = 0
+		for song in training_set:
+		 try:
+		     lossFunc += loss(song[2], genres[song[0]], weights)/D
+		     # choose random vector element and update the gradient for that
+		     random_song = random.sample(training_set, 1)[0]
+		     increment_weight(random_song[2], genres[random_song[0]], weights)
+		 except KeyError:
+		         # skip that example
+		         pass
+		# print "i = ",i,", loss = ", lossFunc
+	return weights
+
+def predict_genre_sgd(weights, x, genre_labels):
+	"""
+	@param list weights: A list of dict objects with the weights for each genre
+	@param defaultdict x: Feature vector of the song (i.e. occurences of words)
+	This function returns the predicted genre as a string, i.e. one of genre_labels
+	Which is the one which gives the max dotProduct with the corresonding genre's weight
+	"""
+	_, i = max((dotProduct(weight, x), i) for i, weight in enumerate(weights))
+	return genre_labels[i]
+
+def sgd_performance(weights, testdata):
+	"""
+	@param list weights: A list of dict objects with the weights for each genre
+	@param testdata: The testdata in the form
+	"""
